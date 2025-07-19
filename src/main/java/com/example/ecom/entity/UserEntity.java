c08e4a55384c1948e97af9a100cc09644007ebcf
mahmoudgad-hub <mgad87@hotmail.com>
2025-07-19 17:25:30 +0300

    commit

--- src/main/java/com/example/ecom/entity/UserEntity.java
+++ src/main/java/com/example/ecom/entity/UserEntity.java
@@ -16,7 +16,7 @@ import java.time.LocalDateTime;
 import java.util.*;
 
 @Entity
-@Table(name = "ECOM_USERS_TAB", schema = "hr")
+@Table(name = "ECOM_USERS_TAB" )
 @EntityListeners(UserAuditListener.class)
 @Setter
 @Getter
@@ -46,7 +46,7 @@ import java.util.*;
 public class UserEntity extends AuditEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY, generator = "USER_SEQ_GENERATOR")
-    @SequenceGenerator(name = "USER_SEQ_GENERATOR", sequenceName = "hr.ECOM_USERS_TAB_SEQ", allocationSize = 1)
+    @SequenceGenerator(name = "USER_SEQ_GENERATOR", sequenceName = "A234491B.ECOM_USERS_TAB_SEQ", allocationSize = 1)
     @Column(name = "USER_ID")
     private Long userId;
 
