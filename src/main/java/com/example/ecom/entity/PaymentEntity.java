f4e1685142c4be4a0379c57c2893aec3bff1a5bd
mahmoudgad-hub <mgad87@hotmail.com>
2025-07-21 23:58:25 +0300

    commit

--- src/main/java/com/example/ecom/entity/PaymentEntity.java
+++ src/main/java/com/example/ecom/entity/PaymentEntity.java
@@ -1,4 +1,38 @@
 package com.example.ecom.entity;
 
-public class PaymentEntity {
+import com.example.ecom.setting.AuditEntity;
+import com.example.ecom.setting.AuditLog.UserAuditListener;
+import com.example.ecom.setting.BooleanToYNConverter;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.Date;
+
+@Entity
+@Table(name = "ECOM_PAYMENT_TAB" )
+@EntityListeners(UserAuditListener.class)
+@Setter
+@Getter
+public class PaymentEntity extends AuditEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY, generator = "PAYMENT_SEQ_GENERATOR")
+    @SequenceGenerator(name = "PAYMENT_SEQ_GENERATOR", sequenceName = "A234491B.ECOM_PAYMENT_TAB_SEQ", allocationSize = 1)
+    @Column(name = "PAYMENT_ID")
+    private Long paymentId;
+
+    private Long userId;
+
+    @Column(name = "CARD_NAME")
+    private String cardName;
+
+    @Column(name = "CARD_NUMBER")
+    private String cardNumber;
+
+    @Column(name = "EXPIRY_DATE")
+    private Date expiryDate;
+
+    @Convert(converter = BooleanToYNConverter.class)
+    private Boolean isActiveFlag;
 }
+
